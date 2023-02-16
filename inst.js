269c4e6cf101277bf1d99d78ed64fd07cb16782e
NegruVictor <120954772+NegruVictor@users.noreply.github.com>
2023-02-16 13:15:39 +0200

    Add files via upload

--- inst.js
+++ inst.js
@@ -1,18 +1,15 @@
-var wordList = new Array; 
+let wordList = new Array; 
 function enter() {
-    var inputs = document.getElementById("listeWord").value;
-    wordList.push(inputs);
-    console.log("The text boxvalue="+document.getElementById('listeWord').value)
+    let input = document.getElementById("listeWord").value;
+    wordList.push(input);
     document.getElementById('listeWord').value = '';
 }
-
 function search() {
-    var wordSearch = document.getElementById("word").value;
-        if(wordList.includes(wordSearch)){
-          alert("the word '" + wordSearch + "' is in the dictionary");
+    let wordSearch = document.getElementById("word").value;
+        if(wordList.includes(wordSearch)) {
+            alert("the word '" + wordSearch + "' is in the dictionary");
         } else {
             alert("the word '" + wordSearch + "' is not in the dictionary");
         }
-        console.log("The text boxvalue="+document.getElementById('word').value)
         document.getElementById('word').value = '';
 }
